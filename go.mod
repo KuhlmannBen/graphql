b975b1acdf0b1ae1db1b329297d80f83a10ffe81
Ben Kuhlmann <private.benkuhlmann@gmail.com>
2020-05-23 00:55:32 +0200

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KuhlmannBen/graphql/server
+
+go 1.14
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const graphql = require('graphql')
-
+    
 const QueryRoot = new graphql.GraphQLObjectType({
-    name: 'Query',
-    fields: () => ({
-      hello: {
-        type: graphql.GraphQLString,
-        resolve: () => "Hello world!"
-      }
-    })
+  name: 'Query',
+  fields: () => ({
+    hello: {
+      type: graphql.GraphQLString,
+      resolve: () => "Hello world!"
+    }
   })
-
-  const schema = new graphql.GraphQLSchema({ query: QueryRoot });
-
-  const app = express();
+})
+    
+const schema = new graphql.GraphQLSchema({ query: QueryRoot });
+    
+const app = express();
 app.use('/api', graphqlHTTP({
   schema: schema,
   graphiql: true,
